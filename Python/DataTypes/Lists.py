e9a5fa2da3d22587e131d367cb01ef9e0d8f4185
Jean-Pierre Gutierrez <jpg@itsdinc.com>
2017-12-29 14:22:52 -0800

    New set of problems

---
+++ Python/DataTypes/Lists.py
@@ -0,0 +1,20 @@
+list = []
+
+def insert():
+# add code
+def print(): 
+# add code
+def remove(): 
+# add code
+def append(): 
+# add code
+def sort(): 
+# add code
+def pop():
+# add code
+def reverse():
+# add code
+
+n = int(input())
+
+print (n)
