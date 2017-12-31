37d7de472781d2bfedb0903edc54f88e9cdfe6dc
Jean-Pierre Gutierrez <jpg@itsdinc.com>
2017-12-31 01:53:20 -0800

    Update

--- Python/DataTypes/Lists.py
+++ Python/DataTypes/Lists.py
@@ -1,20 +1,32 @@
 list = []
-
-def insert():
-# add code
-def print(): 
-# add code
-def remove(): 
-# add code
-def append(): 
-# add code
-def sort(): 
-# add code
-def pop():
-# add code
-def reverse():
-# add code
-
 n = int(input())
+i = 0
+command = ""
 
-print (n)
+while i<n:
+    
+    command = input()
+    command = command.split(" ")
+    
+    if command[0] == 'insert':
+        list.insert(int(command[1]),int(command[2]))
+        
+    elif command[0] == 'print':
+        print(list)
+        
+    elif command[0] == 'remove':
+        list.remove(int(command[1]))
+        
+    elif command[0] == 'append':
+        list.append(int(command[1]))
+        
+    elif command[0] == 'sort':
+        list.sort()
+    
+    elif command[0] == 'pop':
+        list.pop()
+    
+    elif command[0] == 'reverse':
+        list.reverse()
+    	
+    i=i+1
